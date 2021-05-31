19c8d26ad1873fb1ba7dfc1cb17325176f90a145
gowtham024 <gowtham.balan24@gmail.com>
2021-05-31 09:59:43 +0530

    push logo

---
+++ src/org/greens/BasicInfo.java
@@ -0,0 +1,21 @@
+package org.greens;
+
+public class BasicInfo {
+public static void main(String[] args) {
+	String s="GreensTechnology";
+	int lastIndexof=s.lastIndexOf('o');
+	System.out.println(lastIndexof);
+	String a="SeleniumAutomationtools";
+	int indexof=a.indexOf('o');
+	System.out.println(indexof);
+	String b="Velmurugan";
+	int indexof1=b.indexOf('n');
+	System.out.println(indexof1);
+	String c="J a v a p r o g r a m";
+	int LastIndexof1=c.lastIndexOf("emptyspace");
+	System.out.println(LastIndexof1);
+	String d="9095484678";
+	int indexof2=d.indexOf('8');
+	System.out.println(indexof2);
+}
+}
